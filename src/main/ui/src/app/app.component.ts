585afcf2c8b04c557447f55538e80a8448c0c629
delongerr <delongerr@gmail.com>
2018-12-23 02:23:04 +0300

    Add map point

--- src/main/ui/src/app/app.component.ts
+++ src/main/ui/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { AppService} from "./app.service";
 
 declare let ymaps: any;
 
@@ -13,16 +14,33 @@ export class AppComponent implements OnInit {
 
   public map;
 
-  constructor() {}
+  constructor(private appService: AppService) {}
 
   ngOnInit() {
 
     ymaps.ready().then(() => {
-      this.map = new ymaps.Map(this.mapContainer.nativeElement, {
-        center: [56.326887, 44.005986],
-        zoom: 12,
-        controls: ['zoomControl'],
-      });
+
+      this.appService.getStories()
+        .subscribe((data: Array<any>) => {
+
+          this.map = new ymaps.Map(this.mapContainer.nativeElement, {
+            center: [56.326887, 44.005986],
+            zoom: 12,
+          });
+
+          data.map((item) => {
+            const placemark = new ymaps.Placemark([item.x, item.y], {
+              balloonContentHeader: item.name,
+              balloonContentBody: item.details,
+              balloonContentFooter: item.address,
+              hintContent: item.name
+            });
+
+            this.map.geoObjects.add(placemark);
+          });
+        });
+
     });
   }
+
 }
